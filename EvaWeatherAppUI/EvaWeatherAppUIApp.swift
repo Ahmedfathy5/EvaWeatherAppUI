bbdb1caa2b679f91826b5aa82e1f5b3fdd89f37c
Lord Ahmed Fathy <ahmedfathyios1@gmail.com>
2024-07-21 13:43:32 +0300

    Initial Commit

---
+++ EvaWeatherAppUI/EvaWeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvaWeatherAppUIApp.swift
+//  EvaWeatherAppUI
+//
+//  Created by Ahmed Fathi on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EvaWeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
